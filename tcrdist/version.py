89004fc8df2e3f2f816472928792cc74504550a9
kmayerb <kmayerb@gmail.com>
2019-06-17 14:27:31 -0700

    remove include_package_data

--- tcrdist/version.py
+++ tcrdist/version.py
@@ -65,6 +65,5 @@ MAJOR = _version_major
 MINOR = _version_minor
 MICRO = _version_micro
 VERSION = __version__
-INCLUDE_PACKAGE_DATA = True
 PACKAGE_DATA = {'tcrdist': ['datasets/test_human_pairseqs.tsv', 'datasets/test_mouse_pairseqs.tsv']}#[pjoin('datasets', 'test_human_pairseqs.tsv'),pjoin('datasets', 'test_mouse_pairseqs.tsv'), pjoin('db', '*'), pjoin('external', '*')]}
 REQUIRES = ["numpy","pandas","scipy", "matplotlib","parasail"]
