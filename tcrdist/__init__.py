1bba1cc23eb2a5648230364cd68acb53a6b5765b
kmayerb <kmayerb@gmail.com>
2019-10-03 14:43:21 -0700

    make paths absolute

--- tcrdist/__init__.py
+++ tcrdist/__init__.py
@@ -1,5 +1,5 @@
 
-from .version import __version__
+from tcrdist.version import __version__
 
 import logging
 logging.basicConfig(filename='tcrdist.log',
@@ -10,24 +10,24 @@ logging.basicConfig(filename='tcrdist.log',
 logger = logging.getLogger('__init__.py')
 logger.debug('Beginning package imports')
 
-from .hello import *
-from .processing import processNT, computeProbs, samplerProb
-from .tcr_sampler import alpha_cdr3_protseq_probability, beta_cdr3_protseq_probability
-from . import processing
-from . import util # changed from utils to util to match tcrdist
-from . import plotting
-from .objects import TCRClone, TCRChain
-from . import datasets
-from . import distances
-from .all_genes import all_genes
-from .sail import *
-from .setup_blast import install_blast_to_externals
+from tcrdist.hello import *
+from tcrdist.processing import processNT, computeProbs, samplerProb
+from tcrdist.tcr_sampler import alpha_cdr3_protseq_probability, beta_cdr3_protseq_probability
+from tcrdist import processing
+from tcrdist import util # changed from utils to util to match tcrdist
+from tcrdist import plotting
+from tcrdist.objects import TCRClone, TCRChain
+from tcrdist import datasets
+from tcrdist import distances
+from tcrdist.all_genes import all_genes
+from tcrdist.sail import *
+from tcrdist.setup_blast import install_blast_to_externals
 #from .test_resources import adaptive100
-from .pairwise import *
-from . import mappers
-from . import vis_tools
-from . import repertoire_db
-from . import rep_diff as stats
+from tcrdist.pairwise import *
+from tcrdist import mappers
+from tcrdist import vis_tools
+from tcrdist import repertoire_db
+from tcrdist import rep_diff as stats
 
 
 
