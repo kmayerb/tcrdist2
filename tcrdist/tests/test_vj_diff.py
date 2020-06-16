6b185f1508132428c98f7c3233f55b70a0e4f3b7
kmayerb <kmayerb@gmail.com>
2020-06-16 15:42:58 -0700

    Fix bug in vj_diff.py

--- tcrdist/tests/test_vj_diff.py
+++ tcrdist/tests/test_vj_diff.py
@@ -33,14 +33,14 @@ def test_vj_stats():
 
 
 
-ft_testspace = [('vj_occur_freq'),
-                ('v_occur_freq'),
-                ('j_occur_freq'),
-                ('vj_freq'),
-                ('v_freq'),
-                ('j_freq')]
-@pytest.mark.parametrize("ft", ft_testspace)
-def test_vj_stats_on_multiple_metrics(ft):
+ft_testspace = [('vj_occur_freq', ['v_b_gene', 'j_b_gene']),
+                ('v_occur_freq', ['v_b_gene']),
+                ('j_occur_freq', ['j_b_gene']),
+                ('vj_freq', ['v_b_gene', 'j_b_gene']),
+                ('v_freq', ['v_b_gene']),
+                ('j_freq',['j_b_gene'])]
+@pytest.mark.parametrize("ft,cols", ft_testspace)
+def test_vj_stats_on_multiple_metrics(ft, cols):
     fn = os.path.join("tcrdist", "test_files", 'britanova_chord_blood_sample_5000.csv' )
     t = TCRsampler()
     t.ref_df = pd.read_csv(fn).sample(10000, replace = True)
@@ -50,10 +50,30 @@ def test_vj_stats_on_multiple_metrics(ft):
                     freq_type =ft, #'vj_occur_freq',  # <<<<-------
                     df = dfx , 
                     ts = t, 
+                    cols = cols,
                     validate = True)
     print(r)
     assert isinstance(r, list)
     assert isinstance(r[0], float)
+
+ft_testspace = [ ('j_freq',['v_b_gene','j_b_gene']),
+                 ('v_freq',['v_b_gene','j_b_gene']),
+                 ('vj_freq',['j_b_gene'])]
+@pytest.mark.parametrize("ft,cols", ft_testspace)
+def test_vj_stats_on_multiple_metrics_asserterror(ft, cols):
+    fn = os.path.join("tcrdist", "test_files", 'britanova_chord_blood_sample_5000.csv' )
+    t = TCRsampler()
+    t.ref_df = pd.read_csv(fn).sample(10000, replace = True)
+    t.build_background()
+    dfx = t.ref_df.head(10).rename(columns = {'v_reps':'v_b_gene', 'j_reps':'j_b_gene'})
+    with pytest.raises(AssertionError):
+        r = _vj_stats(  chain = 'beta', 
+                    freq_type =ft, #'vj_occur_freq',  # <<<<-------
+                    df = dfx , 
+                    ts = t, 
+                    cols = cols,
+                    validate = True)
+
 
 fn = os.path.join("tcrdist", "test_files", 'britanova_chord_blood_sample_5000.csv' )
 t = TCRsampler()
@@ -67,4 +87,6 @@ ft_testspace = [('TRBV20-1*01' ,'TRBJ2-1*01','vj_occur_freq',t),
                 (None,'TRBJ2-1*01','j_freq',t)]
 @pytest.mark.parametrize("v,j,ft,ts", ft_testspace)
 def test_vj_stat(v,j,ft,ts):
-    _vj_stat(v = v ,j = j, freq_type= ft, ts = ts)
+    r = _vj_stat(v = v ,j = j, freq_type= ft, ts = ts)
+    print(r)
+    assert isinstance(r, float)
--- tcrdist/vj_diff.py
+++ tcrdist/vj_diff.py
@@ -17,6 +17,10 @@ def _vj_stats(chain = 'beta',
     specified by columns < cols > in a Pandas Dataframe < df > , given 
     a tcrsamper instance < ts >
 
+    Make sure you provide appropriate columns with your < freq_type >. 
+    For instance if freq_type = v_freq, cols must specify v-gene names column 
+    only. e.g., cols = ['v_b_gene']
+
     Parameters
     ----------
     chain : str
@@ -30,6 +34,8 @@ def _vj_stats(chain = 'beta',
     validate : bool
         If True, some basic pre-validation is performed on inputs. This is meant for 
         integration with tcrdist2. 
+    cols : list
+        list of column names e.g., ['v_b_gene', 'j_b_gene'] or ['v_b_gene']
 
     Returns
     -------
@@ -48,36 +54,48 @@ def _vj_stats(chain = 'beta',
     >>> fn = os.path.join("tcrdist", "test_files", 'britanova_chord_blood_sample_5000.csv' )
     >>> t.ref_df = pd.read_csv(fn)
     >>> t.build_background()
-    >>> dfx = pd.DataFrame({'v_b_gene':['TRBV20-1*01'],'j_b_gene': ['TRBJ2-1*01']})
-    >>> _vj_stats(df = dfx , ts = t, validate = True, freq_type ='vj_occur_freq', cols = ['v_b_gene', 'j_b_gene'] )
-    [0.0158]
+    >>> dfx = pd.DataFrame({'v_b_gene':['TRBV20-1*01','TRBV20-1*01'],'j_b_gene': ['TRBJ2-1*01','TRBJ2-6*01']})
+    >>> _vj_stats(chain = 'beta', df = dfx , ts = t, freq_type ='vj_occur_freq', cols = ['v_b_gene', 'j_b_gene'],  validate = True )
+    [0.014802960592118424, 0.000600120024004801]
+    >>> _vj_stats(chain = 'beta', df = dfx, ts = t)
+    [0.014802960592118424, 0.000600120024004801]
+    >>> _vj_stats(chain = 'beta', df = dfx, ts = t, cols = ['v_b_gene'], freq_type = 'v_occur_freq')
+    [0.060012002400480095, 0.060012002400480095]
+    >>> _vj_stats(chain = 'beta', df = dfx, ts = t, cols = ['j_b_gene'], freq_type = 'j_occur_freq')
+    [0.272254450890178, 0.013802760552110422]
     """
     tcrdist2_genes =  { 'alpha' :  ['v_a_gene', 'j_a_gene'],
                         'beta'  :  ['v_b_gene', 'j_b_gene'],
                         'gamma'  : ['v_g_gene', 'j_g_gene'],
                         'delta'  : ['v_d_gene', 'j_d_gene']}
-    if validate: # IF validate == True ; THEN do some basic validation of inputs.
-        _vj_stats_validate_inputs(chain = chain, freq_type = freq_type, df=df, ts=ts)
-
     if cols is None:
         cols = tcrdist2_genes[chain]
+    
+    if validate: # IF validate == True ; THEN do some basic validation of inputs.
+        _vj_stats_validate_inputs(chain = chain, freq_type = freq_type, df=df, ts=ts, cols = cols)
+
     v_j = df[cols]
     vjprobs = list()
     for i,r in v_j.iterrows():
         try:
-            k = tuple(r)
-            vjprob = ts.__dict__['vj_occur_freq'][k]
+            if len(cols) == 2:
+                assert freq_type in ['vj_freq', 'vj_occur_freq'], f"TRYING TO ACCESS {freq_type} WITH ONLY V OR J GENE PROVIDED, CHECK < cols > "
+                k = tuple(r[cols].tolist())
+            elif len(cols) == 1:
+                assert freq_type in ['v_freq', 'v_occur_freq','j_freq', 'j_occur_freq'], f"YOU ARE TRYING TO ACCESS {freq_type} WITH BOTH V OR J GENE PROVIDED, CHECK < cols >"
+                k = r[cols].tolist()[0]
+            vjprob = ts.__dict__[freq_type][k]
         except KeyError:
             vjprob = 0.0
         vjprobs.append(vjprob)
     return vjprobs
      
 
-def _vj_stats_validate_inputs(chain,freq_type, df, ts):
+def _vj_stats_validate_inputs(chain,freq_type, df, ts, cols):
     """
     A Pure Validation Routine for _vj_stats to ensure inputs
     are likely to work with tcrdist2 
-    
+
     Parameters
     ----------
     df : pd.DataFrame
@@ -102,6 +120,13 @@ def _vj_stats_validate_inputs(chain,freq_type, df, ts):
     if freq_type not in ['vj_occur_freq', 'vj_freq', 'v_occur_freq', 'v_freq', 'j_occur_freq','j_freq']:
         raise KeyError("freq type must be one of 'vj_occur_freq', 'vj_freq', 'v_occur_freq', 'v_freq', 'j_occur_freq', or 'j_freq'")
     
+    if freq_type in ['vj_occur_freq','vj_freq']:
+        assert len(cols) == 2, f"Given {freq_type}, you must supply two columns, one each for the v and j genes"
+    if freq_type in ['v_occur_freq','v_freq' ]:
+        assert len(cols) == 1, f"Given {freq_type}, you must supply only one appropriate column, for the v gene"
+    if freq_type in ['j_occur_freq','j_freq']:
+        assert len(cols) == 1, f"Given {freq_type}, you must supply only one appropriate column, for and j gene"
+
     if ts is None:
         raise TypeError('_vj_stats argument < ts > must be a tcrsampler.sampler.TCRsampler instance')
     
@@ -155,10 +180,10 @@ def _vj_stat(v = None, j = None,  freq_type = 'vj_occur_freq', ts = None):
         raise ValueError("Niether a v- nor j-gene was supplied to ._vj_stat ; atleast one must be provided")
     
     if v is None:
-        tp = (j)
+        tp = j
         assert freq_type in ['j_freq', 'j_occur_freq']
     elif j is None:
-        tp = (v)
+        tp = v
         assert freq_type in ['v_freq', 'v_occur_freq']
     else:
         tp = (v,j)
