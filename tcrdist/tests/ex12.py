ca5d110120309d8adb0cb34add4ff44143a9be6e
kmayerb <kmayerb@gmail.com>
2020-06-29 11:39:34 -0700

    Change tcrsampler test to test_files

--- tcrdist/tests/ex12.py
+++ tcrdist/tests/ex12.py
@@ -1,9 +1,11 @@
 import pytest 
 
-def test_ex11():
+def test_ex12():
 	import pandas as pd
+	import os
 	from tcrsampler.sampler import TCRsampler
-	fn = 'britanova_chord_blood.csv' 
+	# fn = 'britanova_chord_blood.csv' # real file
+	fn = os.path.join('tcrdist','test_files', 'britanova_chord_blood_sample_5000.csv') # test_only file
 	t = TCRsampler()
 	t.ref_df = pd.read_csv(fn)
 	t.build_background()
